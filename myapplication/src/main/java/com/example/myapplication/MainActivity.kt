9b92720a0be7998219f3eb549e1bd8feb88ac5aa
apokalipsa44 <m_lachowicz@wp.pl>
2020-04-21 23:56:21 +0200

    button listener

--- myapplication/src/main/java/com/example/myapplication/MainActivity.kt
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.kt
@@ -19,6 +19,10 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        btn_gucio.setOnClickListener{
+            btn_gucio.setText("ssss")
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
