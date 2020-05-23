7b0c31c0d860d2e79054c511e9f2aed08cad02ea
apokalipsa44 <m_lachowicz@wp.pl>
2020-05-23 02:32:44 +0200

    todo activity

---
+++ recycler-view/src/main/java/com/michau/recycler_view/TodoItem.kt
@@ -0,0 +1,12 @@
+package com.michau.recycler_view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TodoItem : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo_item)
+    }
+}
