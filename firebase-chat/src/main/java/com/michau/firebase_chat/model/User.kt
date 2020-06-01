64c66a1a2fff5dff436933ca9eb5a88568bfaab4
apokalipsa44 <m_lachowicz@wp.pl>
2020-06-02 01:09:52 +0200

    chat module removed

--- firebase-chat/src/main/java/com/michau/firebase_chat/model/User.kt
+++
@@ -1,9 +0,0 @@
-package com.michau.firebase_chat.model
-
-data class User(val name: String,
-                val bio: String,
-                val profilePicturePatch: String?,
-                val registrationTokens: MutableList<String>){
-    constructor():this("", "", null, mutableListOf())//firestore reqiured
-
-}
